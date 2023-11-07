ef966990acf8556bdd5aede8c03cb8833a846ac2
Daniel Schor <daniel.schor@gmx.de>
2023-11-07 14:58:44 +0100

    Aufgaben

--- SpringIoC/src/main/java/edu/fra/uas/v3autowired/Journeyman.java
+++ SpringIoC/src/main/java/edu/fra/uas/v3autowired/Journeyman.java
@@ -1,16 +1,17 @@
 package edu.fra.uas.v3autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Journeyman {
-//    @Autowired
-//	@Qualifier("pleasePaint")
-//    Work work;
+    @Autowired
+	//@Qualifier("pleasePaint")
+    @Qualifier("pleaseDrill")
+    Work work;
 
     public void performWork() {
-//        work.doWork();
+        work.doWork();
    }
 }
